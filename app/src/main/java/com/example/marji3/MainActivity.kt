1e7ff7c88b0f01edf14cbdb2b5f5d6cb5614f585
said baq <saidbaq1994@gmail.com>
2020-04-21 23:20:39 +0000

    add the code of do next Activity

--- app/src/main/java/com/example/marji3/MainActivity.kt
+++ app/src/main/java/com/example/marji3/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.marji3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        get.setOnClickListener {
+            val intent = Intent(this, LeagueActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
