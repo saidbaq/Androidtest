22a5757a979473c6bc9d523cc2700b59d1c07858
said baq <saidbaq1994@gmail.com>
2020-04-27 23:12:29 +0000

    lesson-07

--- app/src/main/java/com/example/marji3/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/marji3/Controller/LeagueActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.example.marji3.Utilities.EXTRA_LEAGUE
+import com.example.marji3.Model.Player
 import com.example.marji3.R
+import com.example.marji3.Utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
 
-    var selectedLeague = ""
+    var player = Player("", "")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,27 +22,27 @@ class LeagueActivity : BaseActivity() {
         womensLeagueBtn.isChecked = false
         coedLeaugeBtn.isChecked = false
 
-        selectedLeague = "mens"
+        player.leauge = "mens"
     }
 
     fun onWomensClicked(view : View){
         mansLeagueBtn.isChecked = false
         coedLeaugeBtn.isChecked = false
 
-        selectedLeague = "womens"
+        player.leauge = "womens"
     }
 
     fun onCoedClicked(view : View){
         mansLeagueBtn.isChecked = false
         womensLeagueBtn.isChecked = false
 
-        selectedLeague = "co-ed"
+        player.leauge = "co-ed"
     }
 
     fun leagueNextClicked(view: View){
-        if (selectedLeague != ""){
+        if (player.leauge != ""){
             val skillActivity = Intent(this, SkillActivity::class.java)
-            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
+            skillActivity.putExtra(EXTRA_PLAYER, player)
             startActivity(skillActivity)
         } else {
             Toast.makeText(this, "Please select a League", Toast.LENGTH_SHORT).show()
---
+++ app/src/main/java/com/example/marji3/Model/Player.kt
@@ -0,0 +1,32 @@
+package com.example.marji3.Model
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class Player(var leauge: String?, var skill: String?) : Parcelable{
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(leauge)
+        parcel.writeString(skill)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Player> {
+        override fun createFromParcel(parcel: Parcel): Player {
+            return Player(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Player?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
