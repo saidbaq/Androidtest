52c3f2159f8afcce5799088fb402823b14a33b90
said baq <saidbaq1994@gmail.com>
2020-04-28 13:42:12 +0000

    lesson-08

--- app/src/main/java/com/example/marji3/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/marji3/Controller/LeagueActivity.kt
@@ -13,11 +13,22 @@ class LeagueActivity : BaseActivity() {
 
     var player = Player("", "")
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null){
+            player = savedInstanceState.getParcelable<Player?>(EXTRA_PLAYER)!!
+        }
+    }
     fun onMenscClicked(view : View){
         womensLeagueBtn.isChecked = false
         coedLeaugeBtn.isChecked = false
